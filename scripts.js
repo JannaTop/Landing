65fa07a7b43a1ebb2d6833bb2e16a05076f5fdfb
JannaTop <142921367+JannaTop@users.noreply.github.com>
2023-08-22 19:15:31 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -10,3 +10,22 @@ setTimeout(function() {
   }
   
 }, 500);
+
+window.addEventListener('resize', function() {
+  if (window.innerWidth < 768) {
+    var cardsContainer = document.getElementsByClassName('cards');
+    for (var i = 0; i < cardsContainer.length; i++) {
+      cardsContainer[i].style.flexDirection = 'column';
+      cardsContainer[i].style.alignItems = 'center';
+      cardsContainer[i].style.justifyContent = 'center';
+    }
+  }
+  else {
+    var cardsContainer = document.getElementsByClassName('cards');
+    for (var i = 0; i < cardsContainer.length; i++) {
+      cardsContainer[i].style.flexDirection = 'row';
+      cardsContainer[i].style.alignItems = 'unset';
+      cardsContainer[i].style.justifyContent = 'center';
+    }
+  }
+});
